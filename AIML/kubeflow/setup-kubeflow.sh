c49d87683ae2a40d642dff5e25fc55f1e40ca40f
Lokesh Kurre <lokeshkri25@outlook.com>
2023-09-04 01:54:50 +0530

    seprated vscode extension and its setup

--- AIML/kubeflow/setup-kubeflow.sh
+++ AIML/kubeflow/setup-kubeflow.sh
@@ -19,22 +19,4 @@ chmod +x /usr/local/bin/kubectl
 
 curl -L "https://github.com/cdr/code-server/releases/download/${CODESERVER_VERSION}/code-server_${CODESERVER_VERSION/v/}_amd64.deb" -o /tmp/code-server.deb
 dpkg -i /tmp/code-server.deb 
-rm -f /tmp/code-server.deb 
-
-declare -a extensions=("https://marketplace.visualstudio.com/_apis/public/gallery/publishers/dracula-theme/vsextensions/theme-dracula/2.24.3/vspackage"
-    "https://marketplace.visualstudio.com/_apis/public/gallery/publishers/spmeesseman/vsextensions/vscode-taskexplorer/2.13.2/vspackage"
-    "https://marketplace.visualstudio.com/_apis/public/gallery/publishers/ms-python/vsextensions/python/2023.15.12441006/vspackage"
-    "https://marketplace.visualstudio.com/_apis/public/gallery/publishers/tht13/vsextensions/python/0.2.3/vspackage"
-    "https://marketplace.visualstudio.com/_apis/public/gallery/publishers/donjayamanne/vsextensions/python-environment-manager/1.1.1/vspackage"
-    "https://marketplace.visualstudio.com/_apis/public/gallery/publishers/KevinRose/vsextensions/vsc-python-indent/1.18.0/vspackage"
-    "https://marketplace.visualstudio.com/_apis/public/gallery/publishers/VisualStudioExptTeam/vsextensions/vscodeintellicode/1.2.30/vspackage"
-    "https://marketplace.visualstudio.com/_apis/public/gallery/publishers/VisualStudioExptTeam/vsextensions/vscodeintellicode-completions/1.0.22/vspackage"
-    )
-for extension in ${extensions[@]}; do
-    wget -O /tmp/ext.vsix ${extension}
-    code-server --install-extension /tmp/ext.vsix
-    rm -f /tmp/ext.vsix
-done
-code-server --install-extension ms-toolsai.jupyter 
-code-server --install-extension spmeesseman.vscode-taskexplorer 
-code-server --list-extensions --show-versions
+rm -f /tmp/code-server.deb
